a86eb99da004ea2dd70b9b0fc7a5c33abb1e0618
nagyd22 <nagydaniel1112@gmail.com>
2022-12-03 18:42:55 +0100

    Ice try

--- Assets/Scripts/PackController.cs
+++ Assets/Scripts/PackController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PackController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 10;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+// Start is called before the first frame update
+void Start()
+{
+}
+
+// Update is called once per frame
+void Update()
+{
+    if (Input.GetKey("w"))
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+    if (Input.GetKey("d"))
+        transform.Rotate(0.0f, +0.5f, 0.0f);
+
+    if (Input.GetKey("a"))
+        transform.Rotate(0.0f, -0.5f, 0.0f);
+}
 }
