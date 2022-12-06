0dd706e2a1620db0a959837a1a0620c65a381d50
speti-d <solteszpeter98@gmail.com>
2022-12-06 08:17:10 +0100

    Multiply environment

--- Assets/Scripts/PackController.cs
+++ Assets/Scripts/PackController.cs
@@ -11,7 +11,8 @@ public class PackController : Agent
 {
 
     Rigidbody agent_rigidbody;
-    
+    public Transform TargetTransform;
+
     public override void OnEpisodeBegin()
     {
         transform.localPosition = new Vector3(0, 0.5f, -5);
@@ -21,10 +22,26 @@ public class PackController : Agent
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        Debug.Log(actions.ContinuousActions[0] * 90);
-        Debug.Log((Math.Abs(actions.ContinuousActions[1]) * 20 + 5));
+        //Debug.Log(actions.ContinuousActions[0] * 90);
+        //Debug.Log((Math.Abs(actions.ContinuousActions[1]) * 20 + 5));
         transform.Rotate(Vector3.up, actions.ContinuousActions[0] * 90);
         this.agent_rigidbody.AddForce(transform.forward * (Math.Abs(actions.ContinuousActions[1]) * 10 + 10));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Goal")
+        {
+            AddReward(10);
+            EndEpisode();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float distance_scaled = Vector3.Distance(TargetTransform.localPosition, transform.localPosition) / 10;
+        //Debug.Log(distance_scaled);
+        AddReward(-distance_scaled);
     }
 }
 
